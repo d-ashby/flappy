706fd0e6b627381bba96e3d3c439386345176a9c
Daniel Ashby <dashby91@gmail.com>
2022-08-22 17:35:27 +1200

    falling bird

--- flappy-rn/App.js
+++ flappy-rn/App.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import Bird from './components/Bird'
 
 export default function App() {
+  const screenWidth = Dimensions.get('screen').width
+  const screenHeight = Dimensions.get('screen').height
+  const birdLeft = screenWidth / 2
+  const [birdBottom, setBirdBottom] = useState(screenHeight / 2)
+  const gravity = 3
+  let gameTimerId
+
+  // bird gravity
+
+  useEffect(() => {
+    if (birdBottom > 0) {
+      gameTimerId = setInterval(() => {
+        setBirdBottom((birdBottom) => birdBottom - gravity)
+      }, 30)
+
+      return () => {
+        clearInterval(gameTimerId)
+      }
+    }
+  }, [birdBottom])
+
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <Bird />
+      <Bird birdBottom={birdBottom} birdLeft={birdLeft} />
     </View>
   )
 }
