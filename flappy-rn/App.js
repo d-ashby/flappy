72c4c3d52948298ade853c498fc6274e91c1e717
Daniel Ashby <dashby91@gmail.com>
2022-08-22 16:40:19 +1200

    Green Bird

--- flappy-rn/App.js
+++ flappy-rn/App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Bird from './components/Bird'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world!</Text>
+      <Bird />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +18,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ flappy-rn/components/Bird.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Bird = () => {
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        backgroundColor: 'green',
+        width: 50,
+        height: 60,
+      }}
+    />
+  )
+}
+
+export default Bird
